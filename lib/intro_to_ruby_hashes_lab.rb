f3ca3b92815d2e17ca0f56f58ea26e9044d83fba
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-14 20:50:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[key} ? hash[key] += 1 : hash[key} = 1]
+  hash[key] ? hash[key] += 1 : hash[key} = 1]
   hash
   
   # given a hash an a key as parameters, return an updated hash
